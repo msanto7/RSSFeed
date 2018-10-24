aaa65c5f550cced4b000265d62a2fd84c54f671c
MIKE <mike.santoro42@gmail.com>
2018-10-24 13:49:02 -0400

    Added Feed Name Attribute... Toast for Adding

--- app/src/main/java/com/msturtle/mike/rssfeed/AddFeedActivity.java
+++ app/src/main/java/com/msturtle/mike/rssfeed/AddFeedActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddFeedActivity extends AppCompatActivity {
 
@@ -18,6 +21,30 @@ public class AddFeedActivity extends AppCompatActivity {
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button saveButton = (Button) findViewById(R.id.addFeedButton);
+        final EditText feedAddress = (EditText) findViewById(R.id.addFeedEditText);
+        final EditText feedName = (EditText) findViewById(R.id.addFeedNameEditText);
+
+        saveButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(feedAddress.getText().length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Rss Feed Address too short", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Database db = new Database(AddFeedActivity.this);
+
+                RssFeed feed = new RssFeed(feedName.getText().toString(), feedAddress.getText().toString());
+
+                db.addRssFeed(feed);
+                Toast.makeText(getApplicationContext(), "Rss Feed Added!!", Toast.LENGTH_LONG).show();
+                feedAddress.setText("");
+                feedName.setText("");
+
+            }
+        }));
     }
 
 }
