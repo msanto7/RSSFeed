29559cdfc6e6e4abe75d2332c90e6207813a5afd
MIKE <mike.santoro42@gmail.com>
2018-10-24 11:11:43 -0400

    Layout Finished, FloatingButton Action Added

--- app/src/main/java/com/msturtle/mike/rssfeed/MainActivity.java
+++ app/src/main/java/com/msturtle/mike/rssfeed/MainActivity.java
@@ -1,5 +1,6 @@
 package com.msturtle.mike.rssfeed;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(fab.getContext(),AddFeedActivity.class);
+                startActivity(intent);
             }
         });
     }
